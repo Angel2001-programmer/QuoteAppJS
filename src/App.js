56f6912bfcea8695fef12334731dd140ba94358b
Angel2001-programmer <angelwitchell2001@gmail.com>
2023-11-16 14:37:23 +0000

    Quote App completed!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [quote, setQuote] = useState();
+  const [author, setAuthor] = useState();
+  const [isloading, setIsloading] = useState(false);
+
+  useEffect(() => {
+    const getQuote = async () => {
+      const result = await fetch("https://api.quotable.io/rando")
+      if(result.status !== 404){
+      result.json().then(json => {
+            setQuote(json.content);
+            setAuthor(json.author);
+      })
+    }else{
+      alert('API did not load please try again later.')
+      setIsloading(false);
+      setQuote("Quote cannot load.")
+      setAuthor("Author cannot load.")
+    }
+  }
+  if(isloading !== false){
+    try{
+    getQuote();
+    setIsloading(false)
+    }catch(err){
+      console.log(`Api did not load please try again later. ErrorCode: ${err}`);
+    }
+  }
+  console.log(isloading);
+    }, [isloading])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Lets Hear a quote!</p>
+        <p>Quote: {quote}</p>
+        <p>Author: {author}</p>
+        <div className="btnContainer">
+        <button className="QuoteBTN" onClick={() => setIsloading(!isloading)}>New Quote</button>
+        </div>
     </div>
   );
 }
